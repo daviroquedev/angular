3ca8db6a56f35ab14e0978b0aa6d238f9a37e633
Davi Roque <sowdavi@gmail.com>
2023-11-20 00:31:44 -0300

    feat: new exercise angular

--- ex01/app.js
+++ ex01/app.js
@@ -4,7 +4,7 @@
     angular.module('myFirstApp', [])
     
     .controller('MyFirstController', function ($scope) {
-      $scope.name = "Yaakov";
+      $scope.name = "Davi";
       $scope.sayHello = function () {
         return "Hello Coursera!";
       };
